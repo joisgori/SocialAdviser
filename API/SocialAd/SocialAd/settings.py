e97c9060caf9031a44171526a3ef686415980713
CarMer194 <00060313@uca.edu.sv>
2020-10-04 17:05:31 -0600

    Creando conexion con postgresql

--- API/SocialAd/SocialAd/settings.py
+++ API/SocialAd/SocialAd/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
-    'pruebaapi.apps.PruebaapiConfig'
+    'rest_framework',
+    'pruebaapi.apps.PruebaapiConfig',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'SocialAd.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'socialadvisor',
+        'USER': 'socialad',
+        'PASSWORD': 'PP2Social',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
