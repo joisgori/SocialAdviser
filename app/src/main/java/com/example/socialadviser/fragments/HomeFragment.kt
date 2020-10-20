c05a8e9df6b9a06e7a8628f9afc0009d24ca28bf
joisgori <00034715@uca.edu.sv>
2020-10-20 17:40:24 -0600

    implementado el nav_graph, falta arreglar nombre de clases y pulir su implementación

---
+++ app/src/main/java/com/example/socialadviser/fragments/HomeFragment.kt
@@ -0,0 +1,33 @@
+package com.example.socialadviser.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.socialadviser.R
+import kotlinx.android.synthetic.main.fragment_home.view.*
+
+
+class home : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragment_home, container, false)
+
+        view.login.setOnClickListener {
+            this.findNavController().navigate(
+                homeDirections.openRegisterAction()
+            )
+
+            //this.findNavController().navigateUp()
+        }
+
+        return view
+    }
+
+}
