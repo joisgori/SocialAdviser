7be7aa089b67a371b6523c1088d56fcf25fd7602
CarMer194 <00060313@uca.edu.sv>
2020-10-21 10:37:10 -0600

    actualizando panel admin

--- API/SocialAd/pruebaapi/admin.py
+++ API/SocialAd/pruebaapi/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Comercio, Departamento, Municipio, Cliente, Cita, Cola, TipoComercio
+
 
 # Register your models here.
+
+class ClienteAdmin(admin.ModelAdmin):
+    fields = ['correoelectronico_cliente', 'telefono_cliente', 'id_municipio']
+
+
+class ComercioAdmin(admin.ModelAdmin):
+    fields = ['nombre_comercio', 'telefono_comercio', 'movil_comercio',
+              'facebook_comercio', 'messenger_comercio', 'sitioweb_comercio',
+              'descripcion_comercio', 'id_tipocomercio', 'id_municipio', 'id_capacidad']
+
+
+admin.site.register(Comercio, ComercioAdmin)
+admin.site.register(Departamento)
+admin.site.register(Municipio)
+admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(Cita)
+admin.site.register(Cola)
+admin.site.register(TipoComercio)
