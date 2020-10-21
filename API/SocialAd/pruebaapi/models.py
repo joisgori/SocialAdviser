60a0e1de7b3aa41ad7b9015bc7b92b44b7834136
CarMer194 <00060313@uca.edu.sv>
2020-10-21 10:41:27 -0600

    corrigiendo modelos

--- API/SocialAd/pruebaapi/models.py
+++ API/SocialAd/pruebaapi/models.py
@@ -13,8 +13,12 @@ class CapacidadComercio(models.Model):
     cantidad_servidores = models.IntegerField()
     cantidad_max_clientes_cola = models.IntegerField(blank=True, null=True)
 
+    def __str__(self):
+        return self.id_capacidad_comercio.__str__()
+
     class Meta:
         db_table = 'capacidad_comercio'
+
 
 
 class Cita(models.Model):
@@ -23,6 +27,9 @@ class Cita(models.Model):
     id_horario = models.ForeignKey('Horario', on_delete=models.CASCADE, db_column='id_horario')
     id_cliente = models.ForeignKey('Cliente', on_delete=models.CASCADE, db_column='id_cliente')
     id_estado_cita = models.ForeignKey('EstadoCita', on_delete=models.CASCADE, db_column='id_estado_cita')
+
+    def __str__(self):
+        return self.id.__str__()
 
     class Meta:
         db_table = 'cita'
@@ -34,6 +41,11 @@ class Cliente(models.Model):
     telefono_cliente = models.CharField(max_length=9, blank=True, null=True)
     correoelectronico_cliente = models.CharField(max_length=100)
     id_municipio = models.ForeignKey('Municipio', on_delete=models.CASCADE, db_column='id_municipio')
+    nombre_cliente = models.CharField(max_length=200, blank=True, default="")
+    apellido_cliente = models.CharField(max_length=200, blank=True, default="")
+
+    def __str__(self):
+        return self.correoelectronico_cliente
 
     class Meta:
         db_table = 'cliente'
@@ -42,6 +54,9 @@ class Cliente(models.Model):
 class ClienteXCola(models.Model):
     id_cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, db_column='id_cliente')
     id_cola = models.ForeignKey('Cola', on_delete=models.CASCADE, db_column='id_cola')
+
+    def __str__(self):
+        return self.id_cola.__str__()
 
     class Meta:
         db_table = 'cliente_x_cola'
@@ -53,6 +68,9 @@ class Cola(models.Model):
     id_comercio = models.ForeignKey('Comercio', on_delete=models.CASCADE, db_column='id_comercio')
     id_tipo_cola = models.ForeignKey('TipoCola', on_delete=models.CASCADE, db_column='id_tipo_cola')
     id_tipo_servicio = models.ForeignKey('TipoServicio', on_delete=models.CASCADE, db_column='id_tipo_servicio')
+
+    def __str__(self):
+        return self.id_cola.__str__()
 
     class Meta:
         db_table = 'cola'
@@ -72,6 +90,8 @@ class Comercio(models.Model):
     id_municipio = models.ForeignKey('Municipio', on_delete=models.CASCADE, db_column='id_municipio', blank=True, null=True)
     id_capacidad = models.ForeignKey(CapacidadComercio, on_delete=models.CASCADE, db_column='id_capacidad', blank=True,
                                      null=True)
+    def __str__(self):
+        return self.nombre_comercio
 
     class Meta:
         db_table = 'comercio'
@@ -81,6 +101,9 @@ class ComercioXNormativa(models.Model):
     id_normativa = models.ForeignKey('Normativa', on_delete=models.CASCADE, db_column='id_normativa')
     id_comercio = models.ForeignKey(Comercio, on_delete=models.CASCADE, db_column='id_comercio')
 
+    def __str__(self):
+        return self.id_normativa.__str__()
+
     class Meta:
         db_table = 'comercio_x_normativa'
 
@@ -88,6 +111,9 @@ class ComercioXNormativa(models.Model):
 class ComercioXTiposervicio(models.Model):
     id_comercio = models.ForeignKey(Comercio, on_delete=models.CASCADE, db_column='id_comercio')
     id_tiposervicio = models.ForeignKey('TipoServicio', on_delete=models.CASCADE, db_column='id_tiposervicio')
+
+    def __str__(self):
+        return self.id_tiposervicio.__str__()
 
     class Meta:
         db_table = 'comercio_x_tiposervicio'
@@ -97,6 +123,9 @@ class Departamento(models.Model):
     id_departamento = models.IntegerField(primary_key=True)
     nombre_departamento = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.nombre_departamento
+
     class Meta:
         db_table = 'departamento'
 
@@ -105,6 +134,9 @@ class EstadoCita(models.Model):
     id_estado_cita = models.IntegerField(primary_key=True)
     nombre_estado = models.CharField(max_length=50, blank=True, null=True)
     valor_estado = models.BooleanField()
+
+    def __str__(self):
+        return self.nombre_estado
 
     class Meta:
         db_table = 'estado_cita'
@@ -117,6 +149,9 @@ class Horario(models.Model):
     fecha_inicio = models.DateField()
     fecha_final = models.DateField(blank=True, null=True)
 
+    def __str__(self):
+        return self.id_horario.__str__()
+
     class Meta:
         db_table = 'horario'
 
@@ -124,6 +159,9 @@ class Horario(models.Model):
 class HorarioXComercio(models.Model):
     id_horario = models.ForeignKey(Horario, on_delete=models.CASCADE, db_column='id_horario')
     id_comercio = models.ForeignKey(Comercio, on_delete=models.CASCADE, db_column='id_comercio')
+
+    def __str__(self):
+        return self.id_horario.__str__()
 
     class Meta:
         db_table = 'horario_x_comercio'
@@ -133,6 +171,9 @@ class Municipio(models.Model):
     id_municipio = models.IntegerField(primary_key=True)
     nombre_municipio = models.CharField(max_length=100)
     id_departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE, db_column='id_departamento')
+
+    def __str__(self):
+        return self.nombre_municipio
 
     class Meta:
         db_table = 'municipio'
@@ -145,6 +186,9 @@ class Normativa(models.Model):
     id_tipo_normativa = models.ForeignKey('TipoNormativa', on_delete=models.CASCADE, db_column='id_tipo_normativa')
     fecha_de_publicacion_normativa = models.DateField()
 
+    def __str__(self):
+        return self.id_normativa.__str__()
+
     class Meta:
         db_table = 'normativa'
 
@@ -154,6 +198,9 @@ class TipoCola(models.Model):
     nombre_tipo_cola = models.CharField(max_length=200)
     valor_tipo_cola = models.BooleanField()
 
+    def __str__(self):
+        return self.nombre_tipo_cola
+
     class Meta:
         db_table = 'tipo_cola'
 
@@ -161,6 +208,9 @@ class TipoCola(models.Model):
 class TipoComercio(models.Model):
     id_tipo_comercio = models.IntegerField(primary_key=True)
     nombre_tipo_comercio = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre_tipo_comercio
 
     class Meta:
         db_table = 'tipo_comercio'
@@ -170,6 +220,9 @@ class TipoNormativa(models.Model):
     id_normativa = models.IntegerField(primary_key=True)
     nombre_tipo_normativa = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.nombre_tipo_normativa
+
     class Meta:
         db_table = 'tipo_normativa'
 
@@ -177,6 +230,9 @@ class TipoNormativa(models.Model):
 class TipoServicio(models.Model):
     id_tipo_servicio = models.IntegerField(primary_key=True)
     nombre_tipo_servicio = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre_tipo_servicio
 
     class Meta:
         db_table = 'tipo_servicio'
